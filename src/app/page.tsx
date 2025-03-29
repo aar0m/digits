7992a9dafc811eaa1055c25b78f9036f00a018d4
Aaron Ramos <ramosab@hawaii.edu>
2025-03-28 22:50:20 -1000

    landing page mockup finished in 7 minutes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,26 @@ const Home = () => (
       <Container>
         <Row className="align-middle text-center">
           <Col>
-            <PeopleFill size={100} />
-            <h1>Multiple Users</h1>
-            <h5>
+            <PeopleFill size={100} color="white" />
+            <h1 className="text-light">Multiple Users</h1>
+            <h5 className="text-light">
               This address book enables any number of users to register and save their business contacts.
               You can only see the contacts you have created.
             </h5>
           </Col>
 
           <Col>
-            <FileEarmarkTextFill size={100} />
-            <h1>Contact Details</h1>
-            <h5>
+            <FileEarmarkTextFill size={100} color="white" />
+            <h1 className="text-light">Contact Details</h1>
+            <h5 className="text-light">
               For each contact, you can save their name, address, and phone number.
             </h5>
           </Col>
 
           <Col>
-            <Calendar2CheckFill size={100} />
-            <h1>Timestamped Notes</h1>
-            <h5>
+            <Calendar2CheckFill size={100} color="white" />
+            <h1 className="text-light">Timestamped Notes</h1>
+            <h5 className="text-light">
               Each time you make contact with a contact, you can write a note that summarizes the conversation.
               This note is saved along with a timestamp with the contact.
             </h5>
